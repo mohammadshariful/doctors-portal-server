d40c532672497aafc46939853dbde475a2dc9852
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-11 10:58:26 +0600

    node with mongodb init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 500;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rek1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("mongodb connected");
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Doctors Portal Server");
+});
+
+app.listen(port, () => {
+  console.log("doctors portal running port", port);
+});
