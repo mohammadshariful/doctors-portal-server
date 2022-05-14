aaf998a919c988d631f5ccf00a519c8e62c5cdea
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-15 01:20:16 +0600

    all users api created

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+    //get all users
+    app.get("/user", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+
     app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
       const decodedEmail = req.decoded.email;
